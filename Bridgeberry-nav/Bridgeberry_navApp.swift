f80a28cad2d8e8081c9b5228cf86a7f8a870e562
gurusubramaniam <gurutii@gmail.com>
2024-06-10 16:45:57 -0700

    Initial Commit

---
+++ Bridgeberry-nav/Bridgeberry_navApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bridgeberry_navApp.swift
+//  Bridgeberry-nav
+//
+//  Created by Guru S on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Bridgeberry_navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
